8aec184d8aba4248ecda8059e8d627925f152066
Amauri Rossetti Giovani <amauri.giovani@gmail.com>
2024-12-10 20:46:56 -0300

    Changed github account

---
+++ 01-fundamentos-react-ts/src/components/Post.tsx
@@ -0,0 +1,112 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import { pt, ptBR } from 'date-fns/locale'
+import style from './Post.module.css'
+import { Comment } from './Comment'
+import { Avatar } from './Avatar'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
+
+
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+
+export interface PostType {
+    id: number;
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+interface PostProps {
+    post: PostType;
+}
+
+
+export function Post({ post }: PostProps) {
+    const [comments, setComments] = useState(['Post muito bacana'])
+    const [newCommentText, setNewCommentText] = useState('')
+    const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR })
+    const publishedDateRelativeNow = formatDistanceToNow(post.publishedAt, { locale: ptBR, addSuffix: true })
+
+    function handleCreateNewComment(event: FormEvent) {
+        event.preventDefault()
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('')
+        setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('Campo não pode ser nulo')
+    }
+
+    function deleteComment(commentToDelete: string) {
+        const commentsWithoutDeleteOne = comments.filter(comment => {
+            return comment !== commentToDelete
+        })
+        setComments(commentsWithoutDeleteOne)
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
+
+    return (
+        <article className={style.post}>
+            
+            <header>
+                <div className={style.author}>
+                    <Avatar src={post.author.avatarUrl} />
+                    <div className={style.authorInfo}>
+                        <strong>{post.author.name}</strong>
+                        <span>{post.author.role}</span>
+                    </div>
+                </div>
+
+                <time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>{publishedDateRelativeNow}</time>
+            </header>
+
+            <div className={style.content}>
+                {post.content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }
+                })}
+            </div>
+
+            <form onSubmit={handleCreateNewComment} className={style.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    name="comment"
+                    placeholder="Deixe um comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
+                />
+                <footer>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
+                </footer>
+            </form>
+
+            <div className={style.commentList}>
+                {comments.map(comment => {
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
+                })}
+            </div>
+
+        </article>
+    )
+}
---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,73 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import style from './Comment.module.css'
+import { Avatar } from './Avatar'
+import { useState } from 'react'
+
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    // function handleLikeComment() {
+    //     setLikeCount(likeCount + 1)
+    // }
+
+    // E se quiséssemos atualizar de 2 em 2 simplesmente repetindo o código?
+    // function handleLikeComment() {
+    //     setLikeCount(likeCount + 1)
+    //     setLikeCount(likeCount + 1)
+    // }
+    // Não incrementaria de 2 em 2 porque quando o React executa a função acima, 
+    // ele cria um novo contexto (com valor 0) para armazenar o valor em likeCount
+    // e a segunda linha executará o mesmo código com mesmo valor anterior zero (0)
+
+    // Temos duas soluções para solucionar isso
+    // A primeira é criando uma variável armazenando o valor mais atual
+    // Daí quando atualizarmos com + 1, o valor será incrementado
+    // function handleLikeComment() {
+    //     const newLikeCount = likeCount + 1
+    //     setLikeCount(newLikeCount)
+    //     setLikeCount(newLikeCount + 1)
+    // }
+
+    // A segunda e melhor opção é pegar o estado mais atual da variável com 
+    // arrow function "() => {}" e atualizar o estado
+    // É como se utilizasse uma fila de atualizações
+    function handleLikeComment() {
+        setLikeCount((stateLikeCount) => {
+            return stateLikeCount + 1
+        })
+    }
+
+    return (
+        <div className={style.comment}>
+            <Avatar hasBorder={false} src="https://github.com/amaurirg.png" />
+
+            <div className={style.commentBox}>
+                <div className={style.commentContent}>
+                    <header>
+                        <div className={style.authorAndTime}>
+                            <strong>Amauri Rossetti Giovani</strong>
+                            <time title="11 de maio às 08:33h" dateTime="2024-07-09 08:33:47">Cerca de 1h atrás</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    {/* Pode ser usado assim também mas é melhor criar uma função como abaixo */}
+                    {/* <button onClick={() => setLikeCount(likeCount + 1)}> */}
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
